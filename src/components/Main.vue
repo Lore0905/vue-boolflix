56330e7a0bd4c3f6edec2b2a9ec82884d17c5dc4
Lore0905 <lore.castelli4@gmail.com>
2022-01-12 16:37:14 +0100

    POSTER MILESTONE 3 DONE

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,5 @@
-// MILESTONE 2 
-// creo la funzione apiCallSeries
-// faccio la chiamata HTTP --> usando axios 
-// uso i seguenti parameti obbligatori:
-// api_key
-// query
-// creo l' arrey serieDetails nei data 
-// nella funzione .then() --> scrivo che serieDetails deve essere uguale a response.data.results
-// con una props lo passo al componente main
-// inserisco i rispettivi dati
+// MILESTONE 3
+
 <template>
     <main>
         <!-- MOVIES -->
@@ -15,43 +7,48 @@
             <h2>Questi sono i film</h2>
         </div>
 
-        <!-- TITLE -->
-        <div v-for="(film, index) in totalFilmsReserch" :key="index" class="square film">
-            
-            <ul>
-                <li>
-                    <h5>TITLE</h5>
-                </li>
-                <li >
-                    {{film.title}}
-                </li>
-            <!-- TITOLO ORIGINALE -->
-                <li>
-                    <h5>TITOLO ORIGINALE</h5>
-                </li>
-                <li >
-                    {{film.original_title}}
-                </li>
-            <!-- LINGUA -->
-                <li>
-                    <h5>LINGUA</h5>
-                </li>
-                <li >
-                    <div v-if="film.original_language === 'it'  || film.original_language === 'en'" >
-                        <img :src= "require( '../../src/assets/' + film.original_language + '.png')" >
-                    </div>
-                    <div v-else>
-                        {{film.original_language}}
-                    </div>
-                </li>
-            <!-- VOTO -->
-                <li>
-                    <h5>VOTO</h5>
-                </li>
-                <li>
-                    {{film.vote_average}}
-                </li>
-            </ul>
+        <div class="film-wrapping">
+            <div v-for="(film, index) in totalFilmsReserch" :key="index" class="square film">
+                <!-- background -->
+                <div class="background-path" v-if="film.poster_path !==  null">
+                    <img :src="'https://image.tmdb.org/t/p/w300' + film.poster_path" alt="">
+                </div>
+                <!-- TITLE -->
+                <ul>
+                    <li>
+                        <h5>TITLE</h5>
+                    </li>
+                    <li >
+                        {{film.title}}
+                    </li>
+                <!-- TITOLO ORIGINALE -->
+                    <li>
+                        <h5>TITOLO ORIGINALE</h5>
+                    </li>
+                    <li >
+                        {{film.original_title}}
+                    </li>
+                <!-- LINGUA -->
+                    <li>
+                        <h5>LINGUA</h5>
+                    </li>
+                    <li >
+                        <div v-if="film.original_language === 'it'  || film.original_language === 'en'" class="flag">
+                            <img :src= "require( '../../src/assets/' + film.original_language + '.png')" >
+                        </div>
+                        <div v-else>
+                            {{film.original_language}}
+                        </div>
+                    </li>
+                <!-- VOTO -->
+                    <li>
+                        <h5>VOTO</h5>
+                    </li>
+                    <li>
+                        {{film.vote_average}}
+                    </li>
+                </ul>
+            </div>
         </div>
 
         <!-- MOVIES -->
@@ -60,41 +57,47 @@
         </div>
         
         <!-- TITLE -->
-        <div v-for="(serie, index) in totalSerieReserch" :key="index" class="square serie">
-            <ul>
-                <li>
-                    <h5>TITLE</h5>
-                </li>
-                <li >
-                    {{serie.name}}
-                </li>
-            <!-- TITOLO ORIGINALE -->
-                <li>
-                    <h5>TITOLO ORIGINALE</h5>
-                </li>
-                <li >
-                    {{serie.original_name}}
-                </li>
-            <!-- LINGUA -->
-                <li>
-                    <h5>LINGUA</h5>
-                </li>
-                <li >
-                    <div v-if="serie.original_language === 'it'  || serie.original_language === 'en'" >
-                        <img :src= "require( '../../src/assets/' + serie.original_language + '.png')" >
-                    </div>
-                    <div v-else>
-                        {{serie.original_language}}
-                    </div>
-                </li>
-            <!-- VOTO -->
-                <li>
-                    <h5>VOTO</h5>
-                </li>
-                <li>
-                    {{serie.vote_average}}
-                </li>
-            </ul>
+        <div class="serie-wrapping">
+            <div v-for="(serie, index) in totalSerieReserch" :key="index" class="square serie">
+                <!-- background -->
+                <div class="background-path" v-if="serie.poster_path !==  null">
+                    <img :src="'https://image.tmdb.org/t/p/w300' + serie.poster_path" alt="">
+                </div>
+                <ul>
+                    <li>
+                        <h5>TITLE</h5>
+                    </li>
+                    <li >
+                        {{serie.name}}
+                    </li>
+                <!-- TITOLO ORIGINALE -->
+                    <li>
+                        <h5>TITOLO ORIGINALE</h5>
+                    </li>
+                    <li >
+                        {{serie.original_name}}
+                    </li>
+                <!-- LINGUA -->
+                    <li>
+                        <h5>LINGUA</h5>
+                    </li>
+                    <li >
+                        <div v-if="serie.original_language === 'it'  || serie.original_language === 'en'" class="flag">
+                            <img :src= "require( '../../src/assets/' + serie.original_language + '.png')" >
+                        </div>
+                        <div v-else>
+                            {{serie.original_language}}
+                        </div>
+                    </li>
+                <!-- VOTO -->
+                    <li>
+                        <h5>VOTO</h5>
+                    </li>
+                    <li>
+                        {{serie.vote_average}}
+                    </li>
+                </ul>
+            </div>
         </div>
         
     </main>
@@ -112,37 +115,57 @@ export default {
 
 <style scoped lang="scss">
 main{
-    height: calc(100vh - 100px);
-    border: 1px solid black;
-    display: flex;
-    justify-content: space-evenly;
-    align-content: flex-start;
-    flex-wrap: wrap;
-    text-align: center;
+    // height: calc(100vh - 100px);
        .square{
-           border: 1px solid black;
-           width: 150px ;
-           height: 170px;
+           width: 350px;
+           min-height: 500px;
            margin: 20px;
+           position: relative;
+           
+           .background-path{ 
+                img{
+                width: 100%;
+                height: 100%;
+                position: absolute;
+                object-fit: cover;
+                } 
+            } 
             ul{
                 margin: 0;
-                padding: 0;
+                padding: 20px 10px;
+                
                 li{
-                    list-style-type: none;
+                    
                     h5{
                         margin: 3px 0;
+                        padding: 5px 0px;
                     }
-                    img{
+                    .flag{
+                        img {
                         width: 20px;
+                        
+                        }
                     }
                 }
             }
-       }
+        }
 }
+
+// wrapping
+.serie-wrapping, .film-wrapping{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+// single details
 .film{
     background-color: chocolate;
 }
 .serie{
     background-color: cornflowerblue;
 }
+h2{
+    text-align: center;
+}
+     
 </style>
